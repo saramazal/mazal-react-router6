1df9af8f3bb1d14f9f8bfaed0cfa873278d9311c
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-04-14 14:38:42 +0300

    filter checkbox

---
+++ src/components/BlogFilter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blogfilter = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Blogfilter;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -6,12 +6,22 @@ export default function Blog() {
   const [searchParams, setSearchParams] = useSearchParams();
    
 const postQuery = searchParams.get('post') || '';
+const latest = searchParams.has('latest');
+
+const startsFrom = latest ? 80 : 1;
+
 const handleSubmit = (e) => {
   e.preventDefault(); 
   const form = e.target;
-  const query = form.search.value;
 
-  setSearchParams({post: query})
+  const query = form.search.value;
+  const isLatest = form.latest.checked;
+
+  const params = {};
+  if (query.length) params.post = query;
+  if (isLatest) params.latest = true;
+
+  setSearchParams(params);
 }
 
 
@@ -26,13 +36,16 @@ const handleSubmit = (e) => {
       <h1> Our News </h1> 
       <form autoComplete="off" onSubmit={handleSubmit}>
         <input type="search" name="search" />
+        <label className="latest">
+          <input type="checkbox" name="latest" /> New Only
+        </label>
         <input type="submit" value="Search" />
       </form>
       <Link className = 'edit'
       to = '/posts/new'> Add a New Post 
       </Link> 
       { posts.filter(
-        post => post.title.includes(postQuery)
+        post => post.title.includes(postQuery) && post.id >= startsFrom
         ).map(post => ( 
         <Link key = {post.id}
         to = {`/posts/${post.id}`} >
