2162c73643e8ecee3077e62a18dffb201effbd1f
Sara Mazal <32996161+saramazal@users.noreply.github.com>
2022-04-14 14:11:41 +0300

    woking with search

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,10 +3,16 @@ import { Link, useSearchParams } from 'react-router-dom'
 
 export default function Blog() {
   const [posts, setPosts] = useState([]);
-  const [searchParams, setSearchParams] = useState();
+  const [searchParams, setSearchParams] = useSearchParams();
    
-const postquery = searchParams.get('post') || '';
+const postQuery = searchParams.get('post') || '';
+const handleSubmit = (e) => {
+  e.preventDefault(); 
+  const form = e.target;
+  const query = form.search.value;
 
+  setSearchParams({post: query})
+}
 
 
   useEffect(() => {
@@ -18,10 +24,17 @@ const postquery = searchParams.get('post') || '';
   return ( 
     <div >
       <h1> Our News </h1> 
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <input type="search" name="search" />
+        <input type="submit" value="Search" />
+      </form>
       <Link className = 'edit'
       to = '/posts/new'> Add a New Post 
       </Link> 
-      { posts.map(post => ( <Link key = {post.id}
+      { posts.filter(
+        post => post.title.includes(postQuery)
+        ).map(post => ( 
+        <Link key = {post.id}
         to = {`/posts/${post.id}`} >
         <li className = "post" > {post.title} </li> 
         </Link >
